ae220c98b5716014c28c2fd4dabc9fd6df283d64
Franciney Araújo <137841753+francineyaraujoo@users.noreply.github.com>
2023-10-31 13:00:14 -0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,11 +1,13 @@
 document.getElementById("btmulher").addEventListener("click", function () {
     document.getElementById("header").style.backgroundColor = "rgb(216, 153, 184)";
+    document.getElementById("footer").style.backgroundColor = "rgb(216, 153, 184)";
     document.getElementById("botao").style.backgroundColor = "rgb(216, 153, 184)";
     document.getElementById("resultado").style.backgroundColor = "rgb(216, 153, 184)";
     document.getElementById("mulher"). src = "img/mulher.png";
   }, false);
   document.getElementById("bthomem").addEventListener("click", function () {
     document.getElementById("header").style.backgroundColor = "rgb(149, 191, 233)";
+    document.getElementById("footer").style.backgroundColor = "rgb(149, 191, 233)";
     document.getElementById("botao").style.backgroundColor = "rgb(149, 191, 233)";
     document.getElementById("resultado").style.backgroundColor = "rgb(149, 191, 233)";
     document.getElementById("mulher"). src = "img/homem.png";
@@ -22,13 +24,13 @@ document.getElementById("btmulher").addEventListener("click", function () {
       imc = imc.toFixed(2);
 
       if (imc < 18.5) {
-        imcElement.innerHTML =  "Seu IMC: Abaixo do peso " + imc;
+        imcElement.innerHTML =  "Seu IMC: <br><br> Abaixo do peso " + imc;
       } else if (imc >= 18.5 && imc < 25) {
-        imcElement.innerHTML = "Seu IMC: Peso normal " + imc;
+        imcElement.innerHTML = "Seu IMC: <br><br> Peso normal " + imc;
       } else if (imc >= 25 && imc < 30) {
-        imcElement.innerHTML = "Seu IMC: Acima do peso " + imc;
+        imcElement.innerHTML = "Seu IMC:<br><br> Acima do peso " + imc;
       } else {
-        imcElement.innerHTML = "Seu IMC: Obesidade Grau 1 " + imc;
+        imcElement.innerHTML = "Seu IMC: <br><br> Obesidade Grau 1 " + imc;
       }
       imcElement.classList.add("imc-color");
     } else {
@@ -36,5 +38,6 @@ document.getElementById("btmulher").addEventListener("click", function () {
       imcElement.classList.remove("imc-color");
     }
 
-    imcElement.style.fontSize = '50px';
+    imcElement.style.fontSize = '40px';
+    
   }
