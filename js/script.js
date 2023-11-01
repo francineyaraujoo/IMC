ef94cc2630b58ce4d4b6e767413c4149ff8f8583
FrancineyAraujo <137841753+francineyaraujoo@users.noreply.github.com>
2023-11-01 13:55:25 -0300

    IMC Atualizado

--- js/script.js
+++ js/script.js
@@ -14,6 +14,18 @@ document.getElementById("btmulher").addEventListener("click", function () {
   }, false);
 
   function imc() {
+    document.getElementById("r1p").style.backgroundColor = "white";
+    document.getElementById("r1i").style.backgroundColor = "white";
+    document.getElementById("r2p").style.backgroundColor = "white";
+    document.getElementById("r2i").style.backgroundColor = "white";
+    document.getElementById("r3p").style.backgroundColor = "white";
+    document.getElementById("r3i").style.backgroundColor = "white";
+    document.getElementById("r4p").style.backgroundColor = "white";
+    document.getElementById("r4i").style.backgroundColor = "white";
+    document.getElementById("r5p").style.backgroundColor = "white";
+    document.getElementById("r5i").style.backgroundColor = "white";
+    document.getElementById("r6p").style.backgroundColor = "white";
+    document.getElementById("r6i").style.backgroundColor = "white";
     var peso = Number(document.getElementById("peso").value);
     var altura = Number(document.getElementById("altura").value);
     var imc = peso / (altura * altura);
@@ -24,13 +36,29 @@ document.getElementById("btmulher").addEventListener("click", function () {
       imc = imc.toFixed(2);
 
       if (imc < 18.5) {
-        imcElement.innerHTML =  "Seu IMC: <br><br> Abaixo do peso " + imc;
-      } else if (imc >= 18.5 && imc < 25) {
-        imcElement.innerHTML = "Seu IMC: <br><br> Peso normal " + imc;
-      } else if (imc >= 25 && imc < 30) {
-        imcElement.innerHTML = "Seu IMC:<br><br> Acima do peso " + imc;
+        imcElement.innerHTML =  "Seu IMC: <br> Abaixo do peso " + imc;
+        document.getElementById("r1p").style.backgroundColor = "rgb(149, 191, 233)";
+        document.getElementById("r1i").style.backgroundColor = "rgb(149, 191, 233)";
+      } else if (imc >= 18.5 && imc < 24.9) {
+        imcElement.innerHTML = "Seu IMC: <br> Peso normal " + imc;
+        document.getElementById("r2p").style.backgroundColor = "rgb(216, 153, 184)";
+        document.getElementById("r2i").style.backgroundColor = "rgb(216, 153, 184)";
+      } else if (imc >= 25.0 && imc < 29.9) {
+        imcElement.innerHTML = "Seu IMC: <br> Acima do peso " + imc;
+        document.getElementById("r3p").style.backgroundColor = "rgb(149, 191, 233)";
+        document.getElementById("r3i").style.backgroundColor = "rgb(149, 191, 233)";
+      } else if (imc >= 30.0 && imc < 34.9) {
+        imcElement.innerHTML = "Seu IMC: <br> Obesidade Grau I " + imc;
+        document.getElementById("r4p").style.backgroundColor = "rgb(149, 191, 233)";
+        document.getElementById("r4i").style.backgroundColor = "rgb(149, 191, 233)";
+      } else if  (imc >= 35.0 && imc < 39.9) {
+        imcElement.innerHTML = "Seu IMC: <br> Obesidade Grau  II " + imc;
+        document.getElementById("r5p").style.backgroundColor = "rgb(216, 153, 184)";
+        document.getElementById("r5i").style.backgroundColor = "rgb(216, 153, 184)";
       } else {
-        imcElement.innerHTML = "Seu IMC: <br><br> Obesidade Grau 1 " + imc;
+        imcElement.innerHTML = "Seu IMC: <br> Obesidade Grau III " + imc;
+        document.getElementById("r6p").style.backgroundColor = "rgb(149, 191, 233)";
+        document.getElementById("r6i").style.backgroundColor = "rgb(149, 191, 233)";
       }
       imcElement.classList.add("imc-color");
     } else {
@@ -38,6 +66,6 @@ document.getElementById("btmulher").addEventListener("click", function () {
       imcElement.classList.remove("imc-color");
     }
 
-    imcElement.style.fontSize = '40px';
+    imcElement.style.fontSize = '20px';
     
   }
